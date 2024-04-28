a6909ed1ae01be09ed3aa91c318ace860e9cafb0
msvarcmajer <msvarcmajer@gmail.com>
2024-04-28 19:45:38 +0200

    Two screens and step counter added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,8 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("io.coil-kt:coil-compose:2.5.0")
+
+    implementation ("androidx.navigation:navigation-compose:2.4.0-alpha10")
+
+
 }
--- app/src/main/java/rma/lv1/MainActivity.kt
+++ app/src/main/java/rma/lv1/MainActivity.kt
@@ -1,5 +1,10 @@
 package rma.lv1
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,7 +21,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -25,89 +30,167 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.google.firebase.firestore.firestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import rma.lv1.ui.theme.LV1Theme
-import java.security.AllPermission
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import androidx.activity.compose.BackHandler
+import androidx.compose.material3.Button
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LV1Theme {
-                // A surface container using the 'background' color from the theme
+            val navController = rememberNavController()
 
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-
-                ) {
-                    BackgroundImage(modifier = Modifier)
+            // Set up the navigation graph within the NavHost
+            NavHost(navController = navController, startDestination = "main_screen") {
+                composable("main_screen") {
+                    MainScreen(navController = navController)
+                }
+                composable("step_counter") {
+                    StepCounter(navController = navController)
                 }
             }
         }
     }
 }
 
-
+@Composable
+fun MainScreen(navController: NavController) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        BackgroundImage(modifier = Modifier.fillMaxSize())
+        UserPreview()
+        // Button to navigate to StepCounter
+        Button(
+            onClick = {
+                // Navigate to OtherScreen when button clicked
+                navController.navigate("step_counter")
+            },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Text(text = "Step Counter")
+        }
+    }
+}
 
 @Composable
-fun UserPreview(name: String, visina: Float, tezina: Float, modifier: Modifier = Modifier) {
+fun StepCounter(navController: NavController) {
+    var stepCount by remember { mutableStateOf(0) }
+
+    val sensorManager = (LocalContext.current.getSystemService(Context.SENSOR_SERVICE) as SensorManager)
+    val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+    val sensorListener = remember {
+        object : SensorEventListener {
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                // Do nothing
+            }
+
+            override fun onSensorChanged(event: SensorEvent?) {
+                event?.let {
+                    // Calculate step count based on accelerometer data
+                    val x = it.values[0]
+                    val y = it.values[1]
+                    val z = it.values[2]
+
+                    val magnitude = Math.sqrt((x * x + y * y + z * z).toDouble())
+                    if (magnitude > 20) { // Adjust this threshold as needed
+                        stepCount++
+                    }
+                }
+            }
+        }
+    }
+    DisposableEffect(Unit) {
+        sensorManager.registerListener(sensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+        onDispose {
+            sensorManager.unregisterListener(sensorListener)
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        BackgroundImage(modifier = Modifier.fillMaxSize())
+Column {
+    Text(
+        text = "Step Count",
+        fontSize = 20.sp
+    )
+
+    Text(
+        text = stepCount.toString(),
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+        // Back button
+        Button(
+            onClick = { navController.popBackStack() },
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(16.dp)
+        ) {
+            Text("User Info")
+        }
+    }
+
+}
+
+@Composable
+fun UserPreview() {
+
+    val name = "Miljenko"
+    val visina = 1.91f
+    val tezina = 1000f
 
     val bmi = tezina / (visina * visina)
     val formattedBmi = String.format("%.2f", bmi)
 
-    Text(
-        text = "Pozdrav $name!",
-        fontSize = 20.sp,
-        lineHeight = 56.sp,
-        modifier= Modifier
-            .padding(top= 8.dp)
-            .padding(start =10.dp)
-
-    )
-
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
+        modifier = Modifier.padding(16.dp)
     ) {
+        Text(
+            text = "Pozdrav $name!",
+            fontSize = 20.sp,
+            lineHeight = 56.sp,
+            modifier = Modifier.padding(top = 8.dp)
+        )
 
         Text(
             text = "Tvoj BMI je:",
             fontSize = 55.sp,
             lineHeight = 61.sp,
             textAlign = TextAlign.Center,
+        )
 
-
-            )
         Text(
             text = formattedBmi,
             fontSize = 70.sp,
             lineHeight = 72.sp,
             fontWeight = FontWeight.Bold,
         )
-
-
     }
 }
+
 @Composable
 fun BackgroundImage(modifier: Modifier) {
-
-    Box (modifier){ Image(
-        painter = painterResource(id = R.drawable.fitness),
-        contentDescription = null,
-        contentScale = ContentScale.Crop,
-        alpha = 0.1F
-    )
-        UserPreview(name = "Miljenko", visina = 1.91f, tezina =1000f, modifier = Modifier.fillMaxSize())
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.fitness),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.1F
+        )
     }
-
-}
-@Preview(showBackground = false)
-@Composable
-fun UserPreview() {
-    LV1Theme {
-        BackgroundImage(modifier = Modifier)   }
 }
